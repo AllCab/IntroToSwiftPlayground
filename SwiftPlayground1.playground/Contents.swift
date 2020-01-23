90526b24201c73c7a036e327b1318be5d4a5999b
AllCab <59892599+AllCab@users.noreply.github.com>
2020-01-23 10:09:42 -1000

    Completed Assignment #1
    Started Assignment #2

--- SwiftPlayground1.playground/Contents.swift
+++ SwiftPlayground1.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var myFirstVariable = "I've created my first variable"
+
+var mySecondVariable = "and look I added 2 strings together"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
+
+var fourthVar = "Welp, this will be late."
+
+var fifthVar = " Sorry for missing class on tuesday"
+
+var sixthVar = " but I am determined to catch up quickly to not fall behind."
+
+var combined = fourthVar + fifthVar + sixthVar
+
+print(combined)
+//Completed Swift Playgrounds 1, January 23
+
+//Assignment 2
+
+var appState = false
+print(appState)
